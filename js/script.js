2d4d4709b75f58094d8e6c3d32bbbacfb0952cf5
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2022-11-29 10:52:06 +0100

    completed exercise

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
-const buttongenerate = document.getElementById ('generate');
+const button_generate = document.getElementById ('generate');
 let name_client;
 let km;
 let age;
 let price;
 
-generate.addEventListener ('click', function(){
+button_generate.addEventListener ('click', function(){
 
     name_client = document.getElementById('name').value;
-    console.log(name_client)
 
     km = document.getElementById('km').value;
-    console.log(km)
 
     age = document.getElementById('age').value;
-    console.log(age)
 
-    if (age <= 17) {
-        price = (0.21 * km * 0.8).toFixed(2)
-        console.log(price);
-      } else if (age >= 65) {
-        price = (0.21 * km * 0.6).toFixed(2)
-        console.log(price);
-      } else if (age > 17 && age < 65) {
-        price = (0.21 * km).toFixed(2)
-        console.log(price);
-    }
+    if (age == 'minorenne') {
+        price = (0.21 * km * 0.8).toFixed(2);
+        offer = 'Biglietto ridotto under 18';
+      } else if (age == 'over') {
+        price = (0.21 * km * 0.6).toFixed(2);
+        offer= 'Biglietto ridotto over 65';
+      } else if (age == 'maggiorenne') {
+        price = (0.21 * km).toFixed(2);
+        offer= 'Biglietto standard';
+      }
+    
+    document.getElementById ('put_name').innerHTML = name_client;
+    document.getElementById ('put_offer').innerHTML = offer;
+    document.getElementById ('put_price').innerHTML = `€ ${price}`;
 })
